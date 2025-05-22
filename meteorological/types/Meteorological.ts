cf7db524c4fd1570215f87908aaa0c96dd4dea68
Luis-Alvarezz <luisangelug10@gmail.com>
2025-05-21 20:11:17 -0600

    [ADDED]:Clase abstracta que mantiene un orden de implementacion en classes

---
+++ meteorological/types/Meteorological.ts
@@ -0,0 +1,16 @@
+import { Scene } from "three";
+
+// * Interfaz para indicar a las clases la forma o métodos que deben tener
+export default abstract class Meteorological {
+    protected static scene : Scene;
+
+    constructor( scene: Scene) {
+        Meteorological.scene = scene;
+    }
+
+    // Métodos abstractos que implementarán las clases hijas:
+    public abstract init(...args: Array<any>): void; // * Flojo de funcion que recibe cantidad intedeterminada de argumentos y recibe un arreglo de lo que sea
+    public abstract update(...agrs: Array<any>): void;
+    public abstract clear(...agrs: Array<any>): void;
+    
+}
