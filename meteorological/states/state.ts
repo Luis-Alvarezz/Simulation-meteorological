3e163693072d4d31449ed8cc77d138c94b58e4ca
Luis-Alvarezz <luisangelug10@gmail.com>
2025-05-21 20:13:07 -0600

    [ADDED]: Clase abstracta para corroborar y cambiar estados

---
+++ meteorological/states/state.ts
@@ -0,0 +1,15 @@
+export default abstract class State {
+    // * Almacenar el estado que se esta ejecutando:
+    public static currentState: State |null = null;
+
+    public static setCurrent(newState: State) {
+        if (State.currentState === newState) return;
+        
+        if (State.currentState)  State.currentState.exit();
+
+        State.currentState = newState;
+        State.currentState.enter();
+    }
+    public abstract enter(): void;
+    public abstract exit():  void;
+}
