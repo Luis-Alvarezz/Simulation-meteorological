770b252bd584291021adca6a45c97b828f73c35e
Luis-Alvarezz <luisangelug10@gmail.com>
2025-05-21 20:15:26 -0600

    [ADDED]: Metodo para ejecutar los creditos en vista

---
+++ meteorological/states/creditos.state.ts
@@ -0,0 +1,23 @@
+
+import { GUIManager } from "../gui.manager";
+import State from "./state";
+
+export default class CreditState extends State {
+
+
+    public enter(): void {
+        // throw new Error("Method not implemented.");
+        GUIManager.creditButtons();
+        setTimeout(() => {
+            GUIManager.showHTML('panel');
+        }, 0)
+    }
+
+    public exit(): void {
+        // throw new Error("Method not implemented.");
+        GUIManager.removeMenuButtons();
+        GUIManager.hiddenHTML('panel'); // * Ocultar GUI
+    }
+}
+
+// * Este codigo es mas particular, dependiendo del contexto de cada estado
