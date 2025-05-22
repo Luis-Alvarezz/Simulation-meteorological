69010a90514e174ac389719c54915f47c455f750
Luis-Alvarezz <luisangelug10@gmail.com>
2025-05-21 22:11:57 -0600

    [MODIFY]: Se elimino constructor porque no se require instancia

--- meteorological/types/Meteorological.ts
+++ meteorological/types/Meteorological.ts
@@ -4,10 +4,6 @@ import { Scene } from "three";
 export default abstract class Meteorological {
     protected static scene : Scene;
 
-    constructor( scene: Scene) {
-        Meteorological.scene = scene;
-    }
-
     // Métodos abstractos que implementarán las clases hijas:
     public abstract init(...args: Array<any>): void; // * Flojo de funcion que recibe cantidad intedeterminada de argumentos y recibe un arreglo de lo que sea
     public abstract update(...agrs: Array<any>): void;
