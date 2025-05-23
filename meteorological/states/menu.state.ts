4c483e5f73fdc71696d83a4c183599c4ac83bf3d
Luis-Alvarezz <luisangelug10@gmail.com>
2025-05-22 23:51:56 -0600

    [MODIFY]: Se cambiaron metodos para integrar efecto de lluvia por defecto

--- meteorological/states/menu.state.ts
+++ meteorological/states/menu.state.ts
@@ -1,17 +1,27 @@
 import Dioarama from "../classes/diorama";
+import Rain from "../classes/Rain";
 import { GUIManager } from "../gui.manager";
 import State from "./state";
 
 export default class MenuState extends State {
+    private static rain: Rain;
     
     public static diorama: Dioarama;
 
     public enter(): void {
+        // this.showMainMenu();
+        this.initRainEffect();
         GUIManager.menuButtons();
     }
 
     public exit(): void {
         GUIManager.removeMenuButtons();
+        MenuState.rain?.clear();
+    }
+
+    private initRainEffect() : void {
+        MenuState.rain = new Rain(); 
+        MenuState.rain.init();
     }
 }
 // * Este codigo es mas particular, dependiendo del contexto de cada estado
